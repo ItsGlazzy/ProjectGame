656f0459cc48ad3894cb7bde93acfaf133e24d4e
ItsGlazzy <Glazzy01@gmail.com>
2023-10-30 05:35:06 +0700

    Move_Bricks

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -4,9 +4,21 @@ using UnityEngine.Tilemaps;
 public class Board : MonoBehaviour
 {
     public Tilemap tilemap { get; private set; }
-    public Piece activePiece {  get; private set; }
+    public Piece activePiece { get; private set; }
+
     public TetrominoData[] tetrominoes;
-    public Vector3Int spawnPosition;
+    public Vector2Int boardSize = new Vector2Int(10, 20);
+    public Vector3Int spawnPosition = new Vector3Int(-1, 8, 0);
+
+
+    public RectInt Bounds
+    {
+        get
+        {
+            Vector2Int position = new Vector2Int(-boardSize.x / 2, -boardSize.y / 2);
+            return new RectInt(position, boardSize);
+        }
+    }
 
     private void Awake()
     {
@@ -38,5 +50,31 @@ public class Board : MonoBehaviour
             Vector3Int tilePosition = piece.cells[i] + piece.position;
             this.tilemap.SetTile(tilePosition, piece.data.tile);
         }
+    }
+    public void Clear(Piece piece)
+    {
+        for (int i = 0; i < piece.cells.Length; i++)
+        {
+            Vector3Int tilePosition = piece.cells[i] + piece.position;
+            tilemap.SetTile(tilePosition, null);
+        }
+    }
+    public bool IsValidPosition(Piece piece, Vector3Int position)
+    {
+        RectInt bounds = this.Bounds;
+        for (int i = 0; i < piece.cells.Length; i++)
+        {
+            Vector3Int tilePosition = piece.cells[i] + position;
+
+            if (!bounds.Contains((Vector2Int)tilePosition))
+            {
+                return false;
+            }
+            if (tilemap.HasTile(tilePosition))
+            {
+                return false;
+            }
+        }
+        return true;
     }
 }

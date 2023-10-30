c5519de424b382d03aa92721d6486631fc682317
ItsGlazzy <Glazzy01@gmail.com>
2023-10-31 01:15:59 +0700

    Wall_Kicks

--- Assets/Scripts/Piece.cs
+++ Assets/Scripts/Piece.cs
@@ -76,7 +76,18 @@ public class Piece : MonoBehaviour
     }
     private void Rotate(int direction)
     {
+        int originalRotation = this.rotationIndex;
         this.rotationIndex = Wrap(this.rotationIndex + direction, 0, 4);
+        ApplyRotationMatrix(direction);
+        
+        if (!TestWallKicks(this.rotationIndex, direction))
+        {
+            this.rotationIndex = originalRotation;
+            ApplyRotationMatrix(-direction);
+        }
+    }
+    private void ApplyRotationMatrix(int direction)
+    {
         for (int i = 0; i < this.cells.Length; i++)
         {
             Vector3 cell = this.cells[i];
@@ -96,8 +107,30 @@ public class Piece : MonoBehaviour
                     y = Mathf.RoundToInt((cell.x * Data.RotationMatrix[2] * direction) + (cell.y * Data.RotationMatrix[3] * direction));
                     break;
             }
-           this.cells[i] = new Vector3Int(x, y, 0);
+            this.cells[i] = new Vector3Int(x, y, 0);
         }
+    }
+    private bool TestWallKicks(int rotationIndex, int rotationDirction)
+    {
+        int wallKickIndex = GetWallKicksIndex(rotationIndex, rotationDirction);
+        for (int i = 0; i < this.data.wallKicks.GetLength(1); i++)
+        { 
+            Vector2Int translation = this.data.wallKicks[wallKickIndex, i];
+            if (Move(translation))
+            {
+                return true;
+            }    
+        }
+        return false;
+    }
+    private int GetWallKicksIndex(int rotationIndex, int rotationDirection)
+    {
+        int wallKickIndex = rotationIndex * 2;
+        if(rotationDirection < 0)
+        {
+            wallKickIndex--;
+        }
+        return Wrap(wallKickIndex, 0, this.data.wallKicks.GetLength(0));
     }
     private int Wrap(int input, int min, int max)
     {
